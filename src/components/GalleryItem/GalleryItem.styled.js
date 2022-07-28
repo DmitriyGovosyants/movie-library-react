e98333bc3570b658239efd28528d8480df3813fe
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-28 09:47:43 +0300

    feat: add more width film

--- src/components/GalleryItem/GalleryItem.styled.js
+++ src/components/GalleryItem/GalleryItem.styled.js
@@ -2,42 +2,23 @@ import styled from "@emotion/styled";
 import { device } from "utils/mediaquery";
 
 export const FilmCard = styled.li`
-  @media ${device.mobileOnly} {
-    display: flex;
-    flex-direction: column;
-    max-width: 440px;
-    :not(:last-child) {
-      margin-bottom: 30px;
-    }
+  flex-basis: calc((100% - 33px) / 3);
+
+  :not(:nth-last-of-type(-n + 3)) {
+    margin-bottom: 18px;
   }
-  
+
+  :not(:nth-of-type(3n)) {
+    margin-right: 16.5px;
+  }
+
   @media ${device.tablet} {
-    flex-basis: calc((100% - 32px) / 2);
-  }
-
-  @media ${device.tabletOnly} {
-    :not(:nth-last-of-type(-n + 2)) {
+    :not(:nth-last-of-type(-n + 3)) {
       margin-bottom: 32px;
-    }
-
-    :nth-of-type(2n + 1) {
-      margin-right: 32px;
     }
   }
 
   @media ${device.desktop} {
-    flex-basis: calc((100% - 33px) / 3);
-
-    :not(:nth-last-of-type(-n + 3)) {
-      margin-bottom: 32px;
-    }
-
-    :not(:nth-of-type(3n)) {
-      margin-right: 16.5px;
-    }
-  }
-
-  @media ${device.desktopM} {
     flex-basis: calc((100% - 66px) / 5);
 
     :not(:nth-last-of-type(-n + 3)) {
@@ -60,7 +41,11 @@ export const FilmCard = styled.li`
 
 export const PosterThumb = styled.div`
   position: relative;
-  margin-bottom: ${({ theme: { spacing } }) => spacing(2)};
+  margin-bottom: ${({ theme: { spacing } }) => spacing(1)};
+
+  @media ${device.tablet} {
+    margin-bottom: ${({ theme: { spacing } }) => spacing(2)};
+  }
 
   @media ${device.desktop} {
     transition: box-shadow ${({theme: {animation}})=> animation.cubicBezier};
@@ -74,16 +59,20 @@ export const PosterThumb = styled.div`
 
 export const Poster = styled.img`
   width: 100%;
-  height: 420px;
+  height: 125px;
   
   object-fit: cover;
 
   @media ${device.mobileL} {
-    height: 640px;
+    height: 205px;
   }
 
   @media ${device.tablet} {
-    height: 465px;
+    height: 340px;
+  }
+
+  @media ${device.desktop} {
+    height: 271px;
   }
 
   @media ${device.desktopM} {
@@ -93,48 +82,64 @@ export const Poster = styled.img`
 
 export const RatingData = styled.span`
   position: absolute;
-  bottom: 5px;
-  left: 5px;
+  bottom: 0;
+  left: 0;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 5px;
+  padding: 3px;
 
-  font-size: 22px;
+  font-size: 8px;
   font-weight: 600;
   color: #fff;
   background-color: red;
   border-radius: 5px;
 
-  @media ${device.mobileOnly} {
-    font-size: 30px;
+  @media ${device.mobileL} {
+    bottom: 5px;
+    left: 5px;
+    padding: 5px;
+    font-size: 12px;
+  }
+
+  @media ${device.tablet} {
+    font-size: 22px;
   }
 `
 
 export const FilmYear = styled.span`
   position: absolute;
-  right: 5px;
-  bottom: 8px;
+  right: 1px;
+  bottom: 3px;
   
   font-weight: 600;
   color: #fff;
   
-  font-size: 20px;
+  font-size: 8px;
   text-shadow: -1px -1px 0 #a82727, 0 -1px 0 #a82727, 1px -1px 0 #a82727, 1px 0 0 #a82727, 1px 1px 0 #a82727, 0 1px 0 #a82727, -1px 1px 0 #a82727, -1px 0 0 #a82727;
 
-  @media ${device.mobileOnly} {
-    font-size: 30px;
+  @media ${device.mobileL} {
+    right: 5px;
+    bottom: 8px;
+    font-size: 12px;
+  }
+
+  @media ${device.tablet} {
+    font-size: 20px;
   }
 `
 
 export const FilmTitle = styled.h2`
-  font-size: ${({ theme: {fontSizes}}) => fontSizes.medium};
+  font-size: 8px;
   font-weight: 500;
   line-height: 1.33;
 
-  @media ${device.mobileOnly} {
-    font-size: 24px;
-    /* text-align: center; */
+  @media ${device.mobileL} {
+    font-size: 12px;
+  }
+
+  @media ${device.tablet} {
+    font-size: 16px;
   }
 `

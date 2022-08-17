1ad3bc2e46ae26c85849c6fee4e8c7d98a648b6f
DmitriyGovosyants <Demiangvs@gmail.com>
2022-08-17 20:33:57 +0300

    fix: modal window size

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,21 +1,19 @@
 import styled from "@emotion/styled";
+import { device } from "utils/mediaquery";
 
 export const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 100%;
   padding: 20px;
 
   background-color: rgba(0, 0, 0, 0.8);
   overflow-y: scroll;
   z-index: 1200;
   animation-name: backdrop;
-  animation-duration: 500ms;
+  animation-duration: 300ms;
   animation-timing-function: ease;
 
   @keyframes backdrop {
@@ -26,6 +24,12 @@ export const Overlay = styled.div`
     100% {
       opacity: 1;
     }
+  }
+
+  @media ${device.desktop} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `
 

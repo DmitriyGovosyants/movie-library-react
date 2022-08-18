2157a9f2b73b792e5fbc7121c956d3a2525c0818
DmitriyGovosyants <Demiangvs@gmail.com>
2022-08-18 10:12:36 +0300

    fix: App place in app

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export * from './App/App';
+export * from '../App';
 export * from './Container/Container.styled';
 export * from './SharedLayout/SharedLayout';
 export * from './Navigation/Navigation';
